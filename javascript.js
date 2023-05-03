bdd0297e76294e5e05046f7261c730b9604b19eb
mr-wontdo <122188740+mr-wontdo@users.noreply.github.com>
2023-05-02 23:37:15 -0500

    Create function getComputerChoice in javascript.js

--- javascript.js
+++ javascript.js
@@ -1,9 +1,22 @@
-// Prompt the user to input rock, paper, scissors
-// Convert the input to lower case and assign to variable userInput
-// Invoke a random number generator for a number between 0 and 1.
-// If the number is less than .33, then assign a 
+// Prompt the player to input 'rock', 'paper', or 'scissors'
+// Convert the input to lower case and assign to variable playerSelection
 
+// Invoke a function to randomly return 'rock', 'paper', 'scissors', and assign the value to variable computerSelection
+//   Invoke a random number generator for a number between 0 and 1
+//       If the number is less than .33, return 'rock'
+//       Else if the number is greater than .66, return 'paper'
+//       Else return 'scissors'
 
+// If playerSelection is 'rock' and computerSelection is 'paper', declare that the computer won.
+// Else if playerSelection is 'rock' and computerSelection is 'scissors', declare that the player won.
+
+// Else if playerSelection is 'paper' and computerSelection is 'rock', declare that the player won.
+// Else if playerSelection is 'paper' and computerSelection is 'scissors', declare that the computer won.
+
+// Else if playerSelection is 'scissors' and computerSelection is 'rock', declare that the computer won.
+// Else if playerSelection is 'scissors' and computerSelection is 'paper', declare that the player won.
+
+// Else return that the game is tied.
 
 
 function getComputerChoice() {
@@ -16,4 +29,3 @@ function getComputerChoice() {
         return "scissors";
     }
 }
-
