3ba9541c66aca70b77a155d7c687808ec3c25967
mr-wontdo <122188740+mr-wontdo@users.noreply.github.com>
2023-05-03 21:52:46 -0500

    Create function playRound() in javascript.js

--- javascript.js
+++ javascript.js
@@ -1,3 +1,21 @@
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "rock" && computerSelection === "paper") {
+        return "You lose! Paper beats rock!";
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "You win! Rock beats scissors!";
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "You win! Paper beats rock!";
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        return "You lose! Scissors beat paper!";
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return "You lose! Rock beats scissors!";
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "You win! Scissors beat paper!"
+    } else {
+        return "Tied!"
+    }
+}
+
 function getPlayerChoice() {
     return prompt("Rock, paper, or scissors?").toLowerCase();
 }
