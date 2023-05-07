617cdda0f25e6e2aa6950f7724404079b20efdd8
mr-wontdo <122188740+mr-wontdo@users.noreply.github.com>
2023-05-06 21:56:39 -0500

    Create function getFinalResult() in javascript.js
    Create function getFinalResult() to determine the winner from
    the player score and the computer score at the end of the game.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,18 @@
+let playerScore = 0;
+let computerScore = 0;
+game();
+
+function game() {
+    playerScore = 0;
+    computerScore = 0;
+    for (let i = 1; i <= 5; i++) {
+        playRound();
+        console.log(`Player Score: ${playerScore}`);
+        console.log(`Computer Score: ${computerScore}`);
+    }
+    getFinalResult(playerScore, computerScore);
+}
+
 function playRound() {
     let playerSelection = getPlayerChoice();
     let computerSelection = getComputerChoice();
@@ -8,19 +23,35 @@ function playRound() {
 
 function getRoundResult(playerSelection, computerSelection) {
     if (playerSelection === "rock" && computerSelection === "paper") {
+        computerScore += 1;
         return "You lose! Paper beats rock!";
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        playerScore += 1;
         return "You win! Rock beats scissors!";
     } else if (playerSelection === "paper" && computerSelection === "rock") {
+        playerScore += 1;
         return "You win! Paper beats rock!";
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        computerScore += 1;
         return "You lose! Scissors beat paper!";
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        computerScore += 1;
         return "You lose! Rock beats scissors!";
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        playerScore += 1;
         return "You win! Scissors beat paper!"
     } else {
         return "Tied!"
+    }
+}
+
+function getFinalResult(playerScore, computerScore) {
+    if (playerScore > computerScore) {
+        console.log("You won the game!");
+    } else if (computerScore > playerScore) {
+        console.log("You lost the game! The computer won!");
+    } else {
+        console.log("It's a tied game!")
     }
 }
 
@@ -38,23 +69,3 @@ function getComputerChoice() {
         return "scissors";
     }
 }
-
-// Prompt the player to input 'rock', 'paper', or 'scissors'
-// Convert the input to lower case and assign to variable playerSelection
-
-// Invoke a function to randomly return 'rock', 'paper', 'scissors', and assign the value to variable computerSelection
-//   Invoke a random number generator for a number between 0 and 1
-//       If the number is less than .33, return 'rock'
-//       Else if the number is greater than .66, return 'paper'
-//       Else return 'scissors'
-
-// If playerSelection is 'rock' and computerSelection is 'paper', declare that the computer won.
-// Else if playerSelection is 'rock' and computerSelection is 'scissors', declare that the player won.
-
-// Else if playerSelection is 'paper' and computerSelection is 'rock', declare that the player won.
-// Else if playerSelection is 'paper' and computerSelection is 'scissors', declare that the computer won.
-
-// Else if playerSelection is 'scissors' and computerSelection is 'rock', declare that the computer won.
-// Else if playerSelection is 'scissors' and computerSelection is 'paper', declare that the player won.
-
-// Else return that the game is tied.
